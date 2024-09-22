7262c0e0263a53568e15e1e33dc7a1324a7a0ecc
Ikuyo Kita <kitakita@disroot.org>
2024-09-22 12:46:42 +0800

    update run script

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@ source venv/bin/activate
 
 export PYXIV_TOKEN="token_123456"
 export PYXIV_PORT=8000
+export PYXIV_SECRET=$(base64 /dev/urandom | head -c 50)
 #export PYXIV_ACCEPTLANG=""
 #export PYXIV_INSTANCENAME=""
 
-gunicorn --bind 0.0.0.0:${PYXIV_PORT} --workers 5 --bind unix:pyxiv.sock pyxiv.wsgi:app
+gunicorn --bind 0.0.0.0:${PYXIV_PORT} --workers 5 --bind unix:pyxiv.sock pyxiv:app
