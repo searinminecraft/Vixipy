a8b49dcb28e24fe4641ec8d294735d303c20c6f7
Ikuyo Kita <kitakita@disroot.org>
2024-10-18 22:06:17 +0800

    ui update

--- pyxiv/app.py
+++ pyxiv/app.py
@@ -134,7 +134,7 @@ def create_app():
         if route in ("static", "proxy", "robots.txt", "favicon.ico"):
             return
 
-        g.version = "1.5"
+        g.version = "1.6-dev_pixivfe-lnf"
         g.instanceName = cfg.PxInstanceName
 
         g.userPxSession = request.cookies.get("PyXivSession")
