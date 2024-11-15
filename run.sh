b423b24202d1b361496cb252af7a7f73f890b1eb
Ikuyo <searinminecraft@outlook.ph>
2024-11-15 09:22:28 +0800

    set default timeout to 120 seconds

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ export PYXIV_SECRET=$(base64 /dev/urandom | head -c 50)
 #export PYXIV_ACCEPTLANG=""
 #export PYXIV_INSTANCENAME=""
 
-gunicorn --bind 0.0.0.0:${PYXIV_PORT} --workers 5 --bind unix:pyxiv.sock pyxiv:app
+gunicorn --bind 0.0.0.0:${PYXIV_PORT} --timeout 120 --workers 5 --bind unix:pyxiv.sock pyxiv:app
