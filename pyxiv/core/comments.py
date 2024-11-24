f98c8680078fbd982dab4391b2017f299d664a3f
Ikuyo Kita <kitakita@disroot.org>
2024-11-24 12:26:28 +0800

    render pixiv emojis as images

--- pyxiv/core/comments.py
+++ pyxiv/core/comments.py
@@ -1,3 +1,5 @@
+from flask import current_app
+
 from ..api import getArtworkComments as _getArtworkComments
 from ..api import postComment as _postComment
 from ..api import postStamp as _postStamp
@@ -5,7 +7,15 @@ from ..classes import Comment
 
 
 def getArtworkComments(_id: int, **kwargs):
-    return [Comment(x) for x in _getArtworkComments(_id, **kwargs)["body"]["comments"]]
+    data = [Comment(x) for x in _getArtworkComments(_id, **kwargs)["body"]["comments"]]
+    for comment in data:
+        for emoji in current_app.config["emojis"]:
+            comment.comment = comment.comment.replace(
+                    f"({emoji})",
+                    f'<img alt="{emoji}" src="/proxy/s.pximg.net/common/images/emoji/{current_app.config["emojis"][emoji]}.png">'
+            )
+
+    return data
 
 
 def postComment(_id: int, authorId: int, comment: str):
