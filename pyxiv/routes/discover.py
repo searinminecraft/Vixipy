a24696ea8bb2f30309f0ec9372ecec34fce0bed9
Ikuyo Kita <kitakita@disroot.org>
2024-12-06 14:41:57 +0800

    discovery: add mode query param

--- pyxiv/routes/discover.py
+++ pyxiv/routes/discover.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from ..core.discovery import getDiscoveryData
 
@@ -8,6 +8,11 @@ discover = Blueprint("discover", __name__, url_prefix="/discover")
 @discover.route("/")
 def discoverMain():
 
-    data = getDiscoveryData("all", 50)
+    mode = request.args.get("mode", "safe")
+
+    if mode not in ("all", "safe", "r18"):
+        raise ValueError(f"Invalid mode: {mode}")
+
+    data = getDiscoveryData(mode, 50)
 
     return render_template("discover.html", data=data)
