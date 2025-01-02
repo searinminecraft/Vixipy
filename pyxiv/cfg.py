6b2cd0b5dabab9bcf1a2bc3c4ff3f3859ebb7e41
Laptop <laptop@maid.zone>
2025-01-02 21:22:29 +0200

    greeting message

--- pyxiv/cfg.py
+++ pyxiv/cfg.py
@@ -1,6 +1,7 @@
 import os
 import random
 
+Version = "2.1"
 AuthlessMode = False
 
 
@@ -21,3 +22,47 @@ PyXivSecret = os.environ.get("PYXIV_SECRET", "ILoveVixipy")
 PxAcceptLang = os.environ.get("PYXIV_ACCEPTLANG", "en-US,en;q=0.5")
 PxInstanceName = os.environ.get("PYXIV_INSTANCENAME", "Vixipy")
 NoR18 = int(os.environ.get("PYXIV_NOR18", 0)) == 1
+GitRev = os.environ.get("GIT_REVISION", "unknown")
+GitRepo = os.environ.get("GIT_REPO", "unknown")
+
+
+# we need this file to ensure this function only runs once, since vixipy runs multiple workers
+# it is removed if you shutdown gracefully
+if not os.path.isfile("pyxiv.running"):
+    with open("pyxiv.running", "w") as file:
+        file.write("yes it is!")
+    print(f"""
+        
+                ⠀⠀⠀⠀⢀⡴⣆⠀⠀⠀⠀⠀⣠⡀⠀⠀⠀⠀⠀⠀⣼⣿⡗⠀⠀⠀⠀
+                ⠀⠀⠀⣠⠟⠀⠘⠷⠶⠶⠶⠾⠉⢳⡄⠀⠀⠀⠀⠀⣧⣿⠀⠀⠀⠀⠀
+                ⠀⠀⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣤⣤⣤⣤⣤⣿⢿⣄⠀⠀⠀⠀
+ __   ____  __  ⠀⠀⡇⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠀⠀⠀⠀⠀⠀⠙⣷⡴⠶⣦
+ \ \ / /\ \/ /  ⠀⠀⢱⡀⠀⠉⠉⠀⠀⠀⠀⠛⠃⠀⢠⡟⠂⠀⠀⢀⣀⣠⣤⠿⠞⠛⠋
+  \ V /  >  <   ⣠⠾⠋⠙⣶⣤⣤⣤⣤⣤⣀⣠⣤⣾⣿⠴⠶⠚⠋⠉⠁⠀⠀⠀⠀⠀⠀
+   \_/  /_/\_\  ⠛⠒⠛⠉⠉⠀⠀⠀⣴⠟⣣⡴⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀v{Version}
+~~~~~~~~~~~~~~~~~~~~~~~~⠛⠛~~~~~~~~~~~~~~~~~~~~~~~~~""") # cute
+    print(f"Vixipy is listening on 127.0.0.1:{os.environ.get('PYXIV_PORT')}")
+    boolean = lambda a: "Yes" if a else "No"
+    the_config = {
+        "Commit": f"{GitRev} ({GitRepo})",
+        "Using account": boolean(not AuthlessMode),
+        "Accept-Language": PxAcceptLang,
+        "Instance name": PxInstanceName,
+        "NoR18": boolean(NoR18),
+    }
+
+    longest = ""
+    for k in the_config:
+        if len(k) > len(longest):
+            longest = k
+
+    sep = " :: "
+    longest += sep
+
+    for k in the_config:
+        print(k, end="")
+        print(" " * (len(longest)-len(k)-len(sep)), end="")
+        print(sep, end="")
+        print(the_config[k])
+    
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
