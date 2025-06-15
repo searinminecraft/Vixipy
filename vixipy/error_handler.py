e89f3e8dc8009ba19a1a227849fc5977c9594f0a
Vyxie <kitakita@disroot.org>
2025-06-15 13:01:56 +0800

    fix broken error handler

--- vixipy/error_handler.py
+++ vixipy/error_handler.py
@@ -13,11 +13,11 @@ async def on_pixiv_error(e: PixivError):
     if request.headers.get("X-Vixipy-Quick-Action") == "true":
         return "", 500, {"X-Pixiv-Status-Code": f"{e.code} {HTTPStatus(e.code).name}"}
 
-    res.headers["X-Pixiv-Status-Code"] = f"{e.code} {HTTPStatus(e.code).name}"
-
     res = await make_response(
         await render_template("error.html", message=str(e))
     )
+
+    res.headers["X-Pixiv-Status-Code"] = f"{e.code} {HTTPStatus(e.code).name}"
 
     return res, 500
 
