08772c63a373f90f95d18d193b349f35b64610b0
Vyxie <kitakita@disroot.org>
2025-08-17 17:03:20 +0800

    Improve the internal server error page

--- vixipy/error_handler.py
+++ vixipy/error_handler.py
@@ -7,23 +7,12 @@ from werkzeug.exceptions import HTTPException
 from http import HTTPStatus
 from aiohttp.client_exceptions import ClientError
 import logging
+import traceback
 
 if TYPE_CHECKING:
     from quart import Quart
 
 log = logging.getLogger("vixipy")
-
-
-async def on_pixiv_error(e: PixivError):
-
-    if request.headers.get("X-Vixipy-Quick-Action") == "true":
-        return "", 500, {"X-Pixiv-Status-Code": f"{e.code} {HTTPStatus(e.code).name}"}
-
-    res = await make_response(await render_template("error.html", message=str(e)))
-
-    res.headers["X-Pixiv-Status-Code"] = f"{e.code} {HTTPStatus(e.code).name}"
-
-    return res, 500
 
 
 async def on_client_error(e: ClientError):
@@ -41,15 +30,12 @@ async def handle_ratelimit_error(e):
 
 async def handle_internal_error(e):
     if isinstance(e, HTTPException):
-        raise
+        return e
     log.exception("Exception occurred here:")
-    return await render_template("internal_server_error.html")
+    return await render_template("internal_server_error.html", traceback=traceback.format_exc())
 
 
 def init_app(app: Quart):
-    app.register_error_handler(PixivError, on_pixiv_error)
     app.register_error_handler(ClientError, on_client_error)
     app.register_error_handler(429, handle_ratelimit_error)
-
-
-#    app.register_error_handler(Exception, handle_internal_error)
+    app.register_error_handler(Exception, handle_internal_error)
